945e8fdec1615c9457bd5beabf0debe2da91d692
yesungyoo <yeseong.yys@gmail.com>
2026-07-02 11:43:33 +0900

    feat: 사용자 및 알림 삭제 배치 모니터링 추가

--- src/main/java/com/sbproject/deokhugam/job/userdelete/UserDeleteJobConfig.java
+++ src/main/java/com/sbproject/deokhugam/job/userdelete/UserDeleteJobConfig.java
@@ -1,8 +1,10 @@
 package com.sbproject.deokhugam.job.userdelete;
 
 import com.sbproject.deokhugam.domain.user.service.UserService;
+import com.sbproject.deokhugam.monitoring.BatchMetrics;
+import com.sbproject.deokhugam.monitoring.BatchMetricsJobExecutionListener;
+
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
@@ -13,7 +15,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.PlatformTransactionManager;
 
-@Slf4j
 @Configuration
 @EnableBatchProcessing
 @RequiredArgsConstructor
@@ -22,24 +23,27 @@ public class UserDeleteJobConfig {
     private final JobRepository jobRepository;
     private final PlatformTransactionManager transactionManager;
     private final UserService userService;
+	private final BatchMetrics batchMetrics;
+	private final BatchMetricsJobExecutionListener batchMetricsJobExecutionListener;
 
-    @Bean(name = "userDeleteJob")
-    public Job userDeleteJob(Step userDeleteStep) {
+
+	@Bean(name = "userDeleteJob")
+    public Job userDeleteJob() {
         return new JobBuilder("userDeleteJob", jobRepository)
-                .start(userDeleteStep)
+				.listener(batchMetricsJobExecutionListener)
+				.start(userDeleteStep())
                 .build();
     }
 
     @Bean
-    public Step userDeleteStep(UserDeleteTasklet tasklet) {
+    public Step userDeleteStep() {
         return new StepBuilder("userDeleteStep", jobRepository)
-                .tasklet(tasklet, transactionManager)
-                .listener(tasklet)
+                .tasklet(userDeleteTasklet(), transactionManager)
                 .build();
     }
 
     @Bean
     public UserDeleteTasklet userDeleteTasklet() {
-        return new UserDeleteTasklet(userService);
+        return new UserDeleteTasklet(userService, batchMetrics);
     }
 }
